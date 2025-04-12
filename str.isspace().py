fcea005f25934a753470a1f9d626529fe2b6a199
hailegiorgis16 <hailegiorgistesfaye16@gmail.com>
2025-04-12 18:23:00 +0300

    isspace

--- str.isspace().py
+++ str.isspace().py
@@ -2,7 +2,7 @@ text = input("Enter a valid input to get valid")
 if text.isspace():
     print(f"valid input, {text}")
 else:
-    print("invalid") # a code that return valid for input like " "
-## second example
+    print("invalid") 
+## second example code
 dash = "    "
 print(dash.isspace()) # returns True value since there is a nothing 
