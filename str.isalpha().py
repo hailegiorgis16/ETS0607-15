42f84d599cfaaf47da9b40c4430c1742330ebb55
hailegiorgis16 <hailegiorgistesfaye16@gmail.com>
2025-04-12 11:57:14 +0300

    isalpha

---
+++ str.isalpha().py
@@ -0,0 +1,5 @@
+name = input("Enter your name (only latter) ")
+if name.isalpha():
+    print(f"Hello, {name}")
+else:
+    print("Please enter only alphabetic character") # if you put alphabetic character it display Hello, your input 
